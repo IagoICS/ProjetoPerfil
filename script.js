882db2c29cd0f56e24d12b63bbf1c42e6c278933
MontanhaCs <iago18052006@gmail.com>
2023-05-10 22:34:19 -0300

    Criando o backend em javascript do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Olá Seja Bem-Vindo")
+
+let p1=prompt("Digite seu NOME:");
+
+let p2=prompt("Digite um PODER que gostaria de ter:");
+
+let p3=prompt("Digite o nome de um VILÃO:");
+
+let p4=prompt("Digite um LUGAR que gostaria de estar:");
+
+let msg= document.getElementById('msg')
+
+msg.innerHTML ='<p>Olá ${p1} o seu poder é/a ${p2} você vai enfrentar o/a ${p3} no em ${p4}'
