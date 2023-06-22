075e42a3903b8f5fada624c34d8cb031204910a4
MontanhaCs <iago18052006@gmail.com>
2023-06-22 19:26:39 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-alert("Olá Seja Bem-Vindo");
+function promptName() {
+    let p1 = prompt("Digite seu NOME:");
+    if (p1 !== null && p1 !== "") {
+        promptPower(p1);
+    }
+}
 
-let p1=prompt("Digite seu NOME:");
+function promptPower(name) {
+    let p2 = prompt("Digite um PODER que gostaria de ter:");
+    if (p2 !== null && p2 !== "") {
+        promptVillain(name, p2);
+    }
+}
 
-let p2=prompt("Digite um PODER que gostaria de ter:");
+function promptVillain(name, power) {
+    let p3 = prompt("Digite o nome de um VILÃO:");
+    if (p3 !== null && p3 !== "") {
+        promptPlace(name, power, p3);
+    }
+}
 
-let p3=prompt("Digite o nome de um VILÃO:");
+function promptPlace(name, power, villain) {
+    let p4 = prompt("Digite um LUGAR que gostaria de estar:");
+    if (p4 !== null && p4 !== "") {
+        displayMessage(name, power, villain, p4);
+    }
+}
 
-let p4=prompt("Digite um LUGAR que gostaria de estar:");
+function displayMessage(name, power, villain, place) {
+    let msg = document.getElementById('msg');
+    msg.innerHTML = `<p>Olá ${name}, o seu poder é/a ${power}. Você vai enfrentar o/a ${villain} no ${place}.</p>`;
+}
 
-let msg= document.getElementById('msg')
-
-msg.innerHTML =`<p>Olá ${p1} o seu poder é/a ${p2} você vai enfrentar o/a ${p3} no em ${p4}`;
+window.onload = function() {
+    alert("Olá! Seja bem-vindo!");
+    promptName();
+};
